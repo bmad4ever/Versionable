b84b9d73450e7b2bd7f556100dbb4467c1dd7bc9
Bruno Madeira <bmad.works@gmail.com>
2024-11-14 19:37:37 +0000

    Adicionar arquivos de projeto.

---
+++ Versionable.Generator/VersionableSourceGenerator.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace Versionable
+{
+    [Generator]
+    public class VersionableSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new VersionableSyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!(context.SyntaxReceiver is VersionableSyntaxReceiver receiver))
+                return;
+
+            foreach (var structDeclaration in receiver.CandidateStructs)
+            {
+                var model = context.Compilation.GetSemanticModel(structDeclaration.SyntaxTree);
+                var structSymbol = model.GetDeclaredSymbol(structDeclaration) as INamedTypeSymbol;
+
+                var generatedCode = GenerateVersionedClass(structSymbol);
+                context.AddSource($"{structSymbol.Name}_Versioned.g.cs", SourceText.From(generatedCode, Encoding.UTF8));
+            }
+        }
+
+        private string GenerateVersionedClass(INamedTypeSymbol structSymbol)
+        {
+            var structName = structSymbol.Name;
+            var members = structSymbol.GetMembers()
+                .Where(m => m.DeclaredAccessibility == Accessibility.Public && !m.IsStatic)
+                .Where(m => m is IPropertySymbol || m is IFieldSymbol)
+                .ToList();
+
+            var builder = new StringBuilder();
+            builder.AppendLine("using System;");
+            builder.AppendLine("using Versionable;");
+            builder.AppendLine("namespace Versionable.Generated");
+            builder.AppendLine("{");
+            builder.AppendLine($"    public class V_{structName}");
+            builder.AppendLine("    {");
+            builder.AppendLine($"        private readonly Versioned<{structName}> _versioned;");
+            builder.AppendLine();
+            builder.AppendLine($"        public V_{structName}(Versioned<{structName}> versioned) => _versioned = versioned;");
+            builder.AppendLine($"        public V_{structName}(ref {structName} item) => _versioned = new Versioned<{structName}>(ref item);");
+            builder.AppendLine();
+
+            foreach (var member in members)
+            {
+                string propName = member.Name;
+                string propType;
+
+                if (member is IPropertySymbol propertySymbol) propType = propertySymbol.Type.ToDisplayString();
+                else if (member is IFieldSymbol fieldSymbol)  propType = fieldSymbol.Type.ToDisplayString();
+                else throw new InvalidOperationException("Unexpected member type");
+                
+                builder.AppendLine($"        public {propType} {propName}");
+                builder.AppendLine("        {");
+                builder.AppendLine($"            get => _versioned.Peek.{propName};");
+                builder.AppendLine($"            set => _versioned.Update((ref {structName} i) => i.{propName} = value);");
+                builder.AppendLine("        }");
+                builder.AppendLine();
+            }
+
+            builder.AppendLine("    }");
+            builder.AppendLine("}");
+            return builder.ToString();
+        }
+    }
+
+    internal class VersionableSyntaxReceiver : ISyntaxReceiver
+    {
+        public List<StructDeclarationSyntax> CandidateStructs { get; } = new List<StructDeclarationSyntax>();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is StructDeclarationSyntax structDeclaration &&
+                structDeclaration.AttributeLists.Any(attrList =>
+                    attrList.Attributes.Any(attr =>
+                        attr.Name.ToString() == "Versionable" ||
+                        attr.Name.ToString() == "Versioning.Versionable")))
+            {
+                CandidateStructs.Add(structDeclaration);
+            }
+        }
+    }
+}
---
+++ Versionable/Versionable.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.CompilerServices;
+using System;
+
+namespace Versionable
+{
+    [AttributeUsage(AttributeTargets.Struct)]
+    public class VersionableAttribute : Attribute { }
+
+#if NET7_0_OR_GREATER
+
+/// <summary>
+/// Wraps a struct so that each time it is altered via the Versioned wrapper its VERSION is incremented.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public ref struct Versioned<T> where T : struct
+{
+    ref T data;
+
+    public int VERSION { get; private set; }
+    public T Peek => data;
+
+
+    public delegate void UpdateFunction(ref T item);
+
+    public void Update(UpdateFunction update_func)
+    {
+        VERSION++;
+        update_func(ref data);
+    }
+
+    public static Versioned<T> operator |(Versioned<T> item, UpdateFunction update_func)
+    {
+        item.Update(update_func);
+        return item;
+    }
+
+    public Versioned(ref T data)
+    {
+        this.data = ref data;
+        VERSION = 0;
+    }
+}
+
+
+#else
+
+    /// <summary>
+    /// Wraps a struct so that each time it is altered via the Versioned wrapper its VERSION is incremented.
+    /// </summary>
+    public unsafe struct Versioned<T> where T : struct
+    {
+        private readonly T* data;  // Pointer to the T instance
+        public int VERSION { get; private set; }
+
+        public delegate void UpdateFunction(ref T item);
+
+        public T Peek => Unsafe.AsRef<T>(data);
+
+
+        public void Update(UpdateFunction update_func)
+        {
+            VERSION++;
+            update_func(ref Unsafe.AsRef<T>(data));
+        }
+
+        public static Versioned<T> operator |(Versioned<T> item, UpdateFunction update_func)
+        {
+            item.Update(update_func);
+            return item;
+        }
+
+        public Versioned(ref T data)
+        {
+            fixed (T* dataPtr = &data)
+            {
+                this.data = dataPtr;
+            }
+            VERSION = 0;
+        }
+    }
+
+#endif
+}
